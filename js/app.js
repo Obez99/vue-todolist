f3599d4234575dba93a20d50ab078c6bff6e159d
Obez99 <giuliogx@live.it>
2021-11-02 16:13:53 +0100

    Adds function that removes a task when a cross is clicked

--- js/app.js
+++ js/app.js
@@ -2,7 +2,15 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: "#app",
+
   data: {
     toDoList: ["Fare la spesa", "Andare in banca", "Comprare il pane"]
+  },
+
+  methods: {
+    deleteTask: function (i) {
+      this.toDoList.splice(i, 1)
+    }
+
   }
 })
