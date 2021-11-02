0d3d65494a1cab1debf0f371bdc399f6a542c050
Obez99 <giuliogx@live.it>
2021-11-02 16:20:08 +0100

    Adds function that adds a new task when add button is pressed

--- js/app.js
+++ js/app.js
@@ -4,12 +4,17 @@ const app = new Vue({
   el: "#app",
 
   data: {
-    toDoList: ["Fare la spesa", "Andare in banca", "Comprare il pane"]
+    toDoList: ["Fare la spesa", "Andare in banca", "Comprare il pane"],
+    newTask: ""
   },
 
   methods: {
     deleteTask: function (i) {
       this.toDoList.splice(i, 1)
+    },
+
+    addTask: function () {
+      this.toDoList.push(this.newTask)
     }
 
   }
